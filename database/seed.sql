9709ec439241f065bad829707a895473724c4d8d
Daikkotsu <diesanz06@gmail.com>
2026-06-13 07:23:48 -0600

    Conecta backend con base de datos MySQL

---
+++ database/seed.sql
@@ -0,0 +1,7 @@
+USE logisticat_db;
+
+INSERT INTO productos (nombre, cantidad, unidad, fecha_ingreso, fecha_caducidad) VALUES
+(`Leche Deslactosada`, 23, `L`, `2026-06-08`, `2026-06-10`),
+(`Queso tipo Philadelphia`, 7, `pieza`, `2026-06-07`, `2026-06-14`),
+(`Café Molido`, 14, `kg`, `2026-06-01`, `2026-09-10`),
+(`Polvo de chocolate para bebida`, 4, `kg`, `2026-05-20`, `2027-03-05`);
